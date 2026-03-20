16efeee7272044ac8e9e00166fb324634a139bf2
Aniket-Vyass <aniketvyas05@gmail.com>
2026-03-20 23:58:00 +0530

    profile page changed

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:small_social_app/features/auth/domain/entities/app_user.dart';
 import 'package:small_social_app/features/auth/presentation/cubits/auth_cubit.dart';
 import 'package:small_social_app/features/home/presentation/cubit/profile_cubit.dart';
@@ -16,55 +17,179 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  //cubits
-  late final authCubit = context.read<AuthCubit>();
-  late final profileCubit = context.read<ProfileCubit>();
+  //final String currentUserId = ;
+  String username = '';
+  String profileImage = '';
+  String postCount = '0';
 
-  //current user
-  late AppUser? currentUser = authCubit.currentUser;
-
-  //on startup,
   @override
   void initState() {
     super.initState();
-
-    //load user profile
-    profileCubit.fetchUserProfile(widget.uid);
+    loadUserData();
+    loadPostData();
   }
 
-  //Build UI
+  //users collection me se current-user ki uid fetch karo, uid me sab info hoti h eg. username, pfp
+  Future<void> loadUserData() async {
+    final doc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+
+    if (mounted) {
+      // "if mounted" is used a security if user exits the page before it opens
+      // only call setState if the widget is still alive, if dead do not call setState()
+      setState(() {
+        username = doc.data()?['username'] ?? 'Profile';
+        profileImage = doc.data()?['profileImage'] ?? '';
+      });
+    }
+  }
+
+  //posts collection me se
+  Future<void> loadPostData() async {
+    final snapshot = await FirebaseFirestore.instance
+        .collection('posts')
+        .where('uid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+        .get();
+
+    if (mounted) {
+      setState(() {
+        postCount = snapshot.docs.length.toString();
+      });
+    }
+  }
+
+  Future<void> pickImage() async {}
+
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ProfileCubit, ProfileState>(
-      builder: (context, state) {
-        //loaded
-        if (state is ProfileLoaded) {
-          //get loaded user
-          final user = state.profileUser;
+    return Scaffold(
+      appBar: AppBar(title: Text(username)),
+      body: DefaultTabController(
+        length: 3,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
 
-          return Scaffold(
-            //APP BAR
-            appBar: AppBar(
-              title: Text(user.name),
-              foregroundColor: Theme.of(context).colorScheme.primary,
-            ),
+                children: [
+                  Column(
+                    children: [
+                      Stack(
+                        children: [
+                          CircleAvatar(
+                            radius: 50,
+                            backgroundImage: profileImage.isNotEmpty
+                                ? NetworkImage(profileImage)
+                                : null,
+                            child: profileImage.isEmpty
+                                ? Icon(Icons.person)
+                                : null,
+                          ),
 
-            //body
-            body: Column(
-              children: [
-                //email
-                Center(child: Text(user.email)),
-              ],
+                          Positioned(
+                            bottom: 02,
+                            right: 05,
+
+                            child: GestureDetector(
+                              onTap: pickImage,
+                              child: CircleAvatar(
+                                radius: 12,
+                                child: Icon(Icons.add, size: 18),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 15),
+                      Text(
+                        username,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 23),
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              'Posts',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              postCount,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 17), //gap btw posts and followers
+                        Column(
+                          children: [
+                            Text(
+                              'Followers',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ), //gap btw followers and followers count
+                            Text(
+                              '0',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 20),
+                        Column(
+                          children: [
+                            Text(
+                              'Following',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ), //gap btw Following and following count
+                            Text(
+                              '0',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          );
-        }
-        //loading..
-        else if (state is ProfileLoading) {
-          return Scaffold(body: Center(child: CircularProgressIndicator()));
-        } else {
-          return const Center(child: Text('no profile found'));
-        }
-      },
+          ],
+        ),
+      ),
     );
   }
 }
